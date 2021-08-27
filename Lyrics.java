a386c4690eb4dc93787ce687f736bbc55c638635
critic1sm <89173144+critic1sm@users.noreply.github.com>
2021-08-26 18:49:27 -0700

    Create Lyrics.java

---
+++ Lyrics.java
@@ -0,0 +1,33 @@
+
+/**
+ * Prints out lyrics.
+ *
+ * @AA Batteries (Alyssa Liu and Annie Liu)
+ * @August19th
+ * 
+ */  
+public class Lyrics
+{
+
+    /**
+     * Prints out lyrics.
+     * @param args Not used
+     **/
+    public static void main(String[] args) {
+        System.out.println("Let me tell you now");
+        System.out.println("Here we go");
+        System.out.println("\nIt might seem crazy what I'm about to say");
+        System.out.println("Sunshine she's here, you can take a break");
+        System.out.println("I'm a hot air balloon that can go to space");
+        System.out.println("With the air, like I don't care baby by the way (come on)");
+        System.out.println("\nBecause I'm happy");
+        System.out.println("Clap along if you feel like a room without a roof");
+        System.out.println("Because I'm happy");
+        System.out.println("Clap along if you feel like happiness is the truth");
+        System.out.println("Because I'm happy");
+        System.out.println("Clap along if you know what happiness is to you");
+        System.out.println("Because I'm happy");
+        System.out.println("Clap along if you feel like that's what you wanna do (hey)");
+        
+    }
+}
